0cfb9ee22e518b4aac9d863bb9e37c7a4a2bc69d
juan david rios <juandavid3196@gmail.com>
2023-07-19 18:56:48 -0500

    hotel-card component and structure, filters section design, main section design, some general design corrections.

---
+++ src/app/shared/components/hotelcard/hotelcard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hotelcard',
+  templateUrl: './hotelcard.component.html',
+  styleUrls: ['./hotelcard.component.scss']
+})
+export class HotelcardComponent {
+
+}
--- src/app/shared/modules/search-module/search-module.module.ts
+++ src/app/shared/modules/search-module/search-module.module.ts
@@ -4,11 +4,13 @@ import { CommonModule } from '@angular/common';
 import { SearchModuleRoutingModule } from './search-module-routing.module';
 import { SearchComponent } from './search/search.component';
 import { HomeModuleModule } from '../home-module/home-module.module';
+import { HotelcardComponent } from '../../components/hotelcard/hotelcard.component';
 
 
 @NgModule({
   declarations: [
     SearchComponent,
+    HotelcardComponent
   ],
   imports: [
     CommonModule,
